55886f798072dd6c6e89ecaf7e89045b201d5aa4
Gabriel McLachlan <75510344+GMcLachlan45@users.noreply.github.com>
2022-09-15 09:37:42 -0700

    add method to sort all the characters

--- app/MainFile.java
+++ app/MainFile.java
@@ -17,10 +17,15 @@ public class MainFile {
 	    System.out.println("Sort as words? (y/n)");
 	    String sortWord = (scan.nextLine());
 	    
+	    if(sortWord.equals("y") || sortWord.equals("Y"))
+	    	result = sortByFirstLetter(input.toString());
+	    else {
+	    	System.out.println("Sort all characters? (y/n)");
+		    String sortChars = (scan.nextLine());
+	    	result = (sortChars.equals("y") || sortChars.equals("Y")) ? sortByAllCharacters(input.toString()) : input.toString();
+	    }
+	    
 	    System.out.println("\nYou entered:\n " + input.toString().toLowerCase());
-	    
-	    result = (sortWord.equals("y") || sortWord.equals("Y")) ? sortByFirstLetter(input.toString()) : input.toString();
-	    
 	    System.out.println("Result:\n" +result);  // Output user input
 		
 	    scan.close(); // Close the scanner to avoid memory leak
@@ -37,6 +42,19 @@ public class MainFile {
 			res.append(strArr[i] + " ");
         }      
 		
+		return res.toString().trim();
+	}
+	
+	// Sort the string's characters. That is, all the characters will be put in alphabetical order
+	public static String sortByAllCharacters(String in) {
+		char[] charArr = in.toLowerCase().toCharArray(); //Take in the string and turn it into an array of chars
+		Arrays.sort(charArr);
+		
+		// Concatenate all of the chars back into a single string
+		StringBuilder res = new StringBuilder();
+		for (int i = 0; i < charArr.length; i++) {
+			res.append(charArr[i]);
+        }  
 		return res.toString().trim();
 	}
 
